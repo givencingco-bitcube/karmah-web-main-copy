600eb1adf4f083605f70ed183df88acf94a906cf
givencingco-bitcube <given@bitcube.tech>
2024-11-13 15:16:28 +0200

    Testing trigger

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: 'build',
-};
-
-export default nextConfig;
+    exportTrailingSlash: true,
+    trailingSlash: true,
+    exportPathMap: async function (
+  
+    ) {
+      return {
+        '/': { page: '/' },
+        '/about-us': { page: '/about-us' },
+        '/contact-us': { page: '/contact-us' },
+        '/_not-found': { page: '/_not-found' },  
+        // '/_not-found': { page: '/_not-found' },
+      }
+    }
+  };
+  
+  export default nextConfig;
+  
